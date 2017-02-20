3cc3fd12259e54080245b635f8952248f991fdc1
hiratsukashu <hiratsukashu@indival.co.jp>
2017-02-20 10:01:30 +0900

    処理の流れはわかったので、次はmecabで試してみる

--- morphological.py
+++ morphological.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 import urllib
-import urllib2
-from BeautifulSoup import BeautifulSoup
+try:
+    import urllib.request as urllib2
+except ImportError:
+    import urllib2
+# from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 
 appid = 'dj0zaiZpPVpXUE9yNG1QODN6aCZzPWNvbnN1bWVyc2VjcmV0Jng9OGQ-'
 pageurl = "http://jlp.yahooapis.jp/MAService/V1/parse";
@@ -9,8 +13,8 @@ pageurl = "http://jlp.yahooapis.jp/MAService/V1/parse";
 # Yahoo!形態素解析の結果をリストで返します。
 def split(sentence, appid=appid, results="ma", filter="1|2|3|4|5|9|10"):
   sentence = sentence.encode("utf-8")
-  params = urllib.urlencode({'appid':appid, 'results':results, 'filter':filter,'sentence':sentence})
+  params = urllib.parse.urlencode({'appid':appid, 'results':results, 'filter':filter,'sentence':sentence}).encode('utf8')
   results = urllib2.urlopen(pageurl, params)
-  soup = BeautifulSoup(results.read())
+  soup = BeautifulSoup(results.read(), 'html.parser')
 
   return [w.surface.string for w in soup.ma_result.word_list]
--- naivebayes.py
+++ naivebayes.py
@@ -7,3 +7,28 @@ import morphological
 def getwords(doc):
     words = [s.lower() for s in morphological.split(doc)]
     return tuple(w for w in words)
+
+class NaiveBayes:
+  def __init__(self):
+    self.vocabularies = set() # 単語の集合
+    self.wordcount = {}       # {category : { words : n, ...}}
+    self.catcount = {}        # {category : n}
+
+  def wordcountup(self, word, cat):
+    self.wordcount.setdefault(cat, {})
+    self.wordcount[cat].setdefault(word, 0)
+    self.wordcount[cat][word] += 1
+    self.vocabularies.add(word)
+
+  def catcountup(self, cat):
+    self.catcount.setdefault(cat, 0)
+    self.catcount[cat] += 1
+
+  def train(self, doc, cat):
+    word = getwords(doc)
+    for w in word:
+      self.wordcountup(w, cat)
+    self.catcountup(cat)
+
+  def priorprob(self, cat):
+    return float(self.catcount[cat]) / sum(self.catcount.values())
